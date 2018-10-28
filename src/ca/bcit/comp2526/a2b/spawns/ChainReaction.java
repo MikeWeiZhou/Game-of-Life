8fb58e1a0fde08766a93e621651bb4949d87576c
Mike Zhou <mikeweizhou@gmail.com>
2018-10-28 03:42:40 -0700

    Updated ChainReaction map ratios to kill off animals/plants faster

--- src/ca/bcit/comp2526/a2b/spawns/ChainReaction.java
+++ src/ca/bcit/comp2526/a2b/spawns/ChainReaction.java
@@ -18,19 +18,39 @@ public class ChainReaction extends Spawn {
      * @param world    that Lifeform will spawn in
      */
     public ChainReaction(final World world) {
+//        super(world);
+//
+//        setUnspawnableTerrain(Terrain.WATER);
+//
+//        addTerraformRate(Terrain.WATER, 0.1f);
+//        addTerraformRate(Terrain.LAND,  0.9f);
+//
+//        addSpawnRate(LifeformType.PLANT,     0.3f);
+//        addSpawnRate(LifeformType.CARNIVORE, 0.06f);
+//        addSpawnRate(LifeformType.HERBIVORE, 0.12f);
+//
+//        addMortalityRate(LifeformType.PLANT,     0.00001f);
+//        addMortalityRate(LifeformType.CARNIVORE, 0.00001f);
+//        addMortalityRate(LifeformType.HERBIVORE, 0.15f);
+
+
+
+
         super(world);
 
         setUnspawnableTerrain(Terrain.WATER);
 
-        addTerraformRate(Terrain.WATER, 0.1f);
-        addTerraformRate(Terrain.LAND,  0.9f);
+        addTerraformRate(Terrain.WATER, 0.04f);
+        addTerraformRate(Terrain.LAND,  0.96f);
 
-        addSpawnRate(LifeformType.PLANT,     0.3f);
-        addSpawnRate(LifeformType.CARNIVORE, 0.06f);
-        addSpawnRate(LifeformType.HERBIVORE, 0.12f);
+        addSpawnRate(LifeformType.PLANT,     0.05f);
+        addSpawnRate(LifeformType.HERBIVORE, 0.14f);
+        addSpawnRate(LifeformType.CARNIVORE, 0.1f);
+        addSpawnRate(LifeformType.OMNIVORE,  0.1f);
 
-        addMortalityRate(LifeformType.PLANT,     0.00001f);
-        addMortalityRate(LifeformType.CARNIVORE, 0.00001f);
-        addMortalityRate(LifeformType.HERBIVORE, 0.15f);
+        addMortalityRate(LifeformType.PLANT,     0.1f);
+        addMortalityRate(LifeformType.HERBIVORE, 0.2f);
+        addMortalityRate(LifeformType.CARNIVORE, 0.0000001f);
+        addMortalityRate(LifeformType.OMNIVORE,  0.000001f);
     }
 }
